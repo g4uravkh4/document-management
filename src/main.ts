b7a82ce33db2200b7a36da53f6b4201eed79aed7
g4kh4 <gauravkhatiwada4@gmail.com>
2026-08-25 15:33:36 +0545

    Expose API root endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { RequestMethod, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { setupSwagger } from './infrastructure/swagger/setup-swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
+  app.setGlobalPrefix('api', {
+    exclude: [{ path: '/', method: RequestMethod.GET }],
+  });
   app.enableCors({
     origin: process.env.CORS_ORIGIN?.split(',').map((value) => value.trim()) ?? true,
     credentials: true,
